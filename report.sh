328439d2fae1a63577462181a49a03ba6d2734cf
HitkoDev <development@hitko.si>
2023-11-15 16:57:23 +0100

    feat: report metadata

--- report.sh
+++ report.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd ./report
-make clean
-make
+apt-get update;
+apt-get install -y locales;
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen;
+/usr/sbin/locale-gen;
+pip install pyyaml --break-system-packages --root-user-action=ignore;
+cd ./report;
+make clean;
+make;
