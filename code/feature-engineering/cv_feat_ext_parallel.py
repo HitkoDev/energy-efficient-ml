a2bb4757a608aba1e14d9fe1b65c6999cf1e13a7
DrejcPesjak <dp8949@student.uni-lj.si>
2024-01-13 17:02:46 +0100

    CV feat. eng. cleanup and full run, MT feat. eng. BPE added

--- code/feature-engineering/cv_feat_ext_parallel.py
+++ code/feature-engineering/cv_feat_ext_parallel.py
@@ -4,25 +4,10 @@ import pandas as pd
 import os
 from skimage import color, feature, measure, filters
 
-def calculate_brightness(img):
-    return np.mean(img)
-
-def calculate_rms(img):
-    return np.sqrt(np.mean(np.square(img)))
-
 def calculate_contrast(img):
     return img.std()
 
 # https://stackoverflow.com/a/3498247
-def calculate_perceived_brightness_avg(img):
-    r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
-    return np.mean(np.sqrt(0.241 * (r**2) + 0.691 * (g**2) + 0.068 * (b**2)))
-
-def calculate_perceived_brightness_rms(img):
-    r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
-    return np.sqrt(np.mean(0.241 * (r**2) + 0.691 * (g**2) + 0.068 * (b**2)))
-
-
 def calculate_perceived_brightness(img, method='avg'):
     if img.ndim == 2 or img.shape[2] == 1:  # If the image is already grayscale
         r = g = b = img
@@ -36,42 +21,10 @@ def calculate_perceived_brightness(img, method='avg'):
     else:
         raise ValueError("Unsupported method")
 
-
-# SIFT Keypoints and Descriptors
-def calculate_keypoints_and_descriptors(image):
-    sift = cv2.SIFT_create()
-    keypoints, descriptors = sift.detectAndCompute(image, None)
-    return keypoints, descriptors
-
 def calculate_keypoints_and_descriptors(image):
     orb = cv2.ORB_create()
     keypoints, descriptors = orb.detectAndCompute(image, None)
     return keypoints, descriptors
-
-# # Hue Histogram
-# def calculate_hue_histogram(img):
-#     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-#     hue_histogram, _ = np.histogram(hsv_img[:, :, 0], bins=7, range=(0, 180))
-#     return hue_histogram
-
-
-def calculate_edges(img):
-    edges = feature.canny(color.rgb2gray(img))
-    edge_lengths = measure.regionprops(measure.label(edges), intensity_image=img)[0].area
-    edge_histogram, _ = np.histogram(edge_lengths, bins=7, range=(1, edge_lengths+1))
-    return edge_histogram
-
-def calculate_edges(img):
-    gray = color.rgb2gray(img)
-    # Apply Gaussian Blur
-    blurred = filters.gaussian(gray, sigma=2)
-    # Find edges using LoG
-    edges = feature.blob_log(blurred, max_sigma=30, num_sigma=10, threshold=0.1)
-    # Calculate the length of each blob detected by LoG
-    edge_lengths = [blob[2] * np.sqrt(2) for blob in edges]  # The blob radius is given by blob[2]*sqrt(2)
-    edge_histogram, _ = np.histogram(edge_lengths, bins=7, range=(0, max(edge_lengths) if edge_lengths else 1))
-    return edge_histogram
-
 
 def calculate_edges_new(img):
     edges = feature.canny(color.rgb2gray(img), sigma=3)
@@ -104,35 +57,6 @@ def calculate_edge_angles(img):
 
     return angle_histogram
 
-# def calculate_edge_features(image):
-#     # Convert to grayscale and detect edges
-#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     edges = cv2.Canny(gray, 100, 200)
-
-#     # Find contours and calculate the length of each contour (edge length)
-#     contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#     edge_lengths = [cv2.arcLength(contour, True) for contour in contours]
-
-#     if edge_lengths:  # Check if the list is not empty
-#         edge_length_histogram, _ = np.histogram(edge_lengths, bins=7, range=(0, max(edge_lengths)+1))
-#     else:  # If no edges are detected, create a histogram with zeros
-#         edge_length_histogram = np.zeros(7)
-
-#     # Calculate gradients for edge orientations
-#     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=5)
-#     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=5)
-
-#     # Calculate edge angles in degrees
-#     edge_angles = np.arctan2(sobely, sobelx) * (180 / np.pi)
-#     edge_angles = np.mod(edge_angles, 180)  # Angles range from 0 to 180 degrees
-
-#     if edges.any():  # Check if there are any edges detected
-#         edge_angle_histogram, _ = np.histogram(edge_angles[edges > 0], bins=7, range=(0, 180))
-#     else:  # If no edges are detected, create a histogram with zeros
-#         edge_angle_histogram = np.zeros(7)
-
-#     return edge_length_histogram, edge_angle_histogram
-
 def calculate_aspect_ratio(contour):
     _, _, w, h = cv2.boundingRect(contour)
     return w / h if h > 0 else 0
@@ -147,122 +71,44 @@ def calculate_hue_histogram(img):
     hue_histogram, _ = np.histogram(hsv_img[:, :, 0], bins=7, range=(0, 180))
     return hue_histogram
 
-def calculate_hue_histogram_RGB(image):
-    def rgb_to_hue(r, g, b):
-        # Normalize RGB values
-        r_norm, g_norm, b_norm = r / 255.0, g / 255.0, b / 255.0
-        max_val = max(r_norm, g_norm, b_norm)
-        min_val = min(r_norm, g_norm, b_norm)
-        delta = max_val - min_val
-
-        if delta == 0:
-            return 0
-
-        if max_val == r_norm:
-            hue = ((g_norm - b_norm) / delta) % 6
-        elif max_val == g_norm:
-            hue = (b_norm - r_norm) / delta + 2
-        else:
-            hue = (r_norm - g_norm) / delta + 4
-
-        hue *= 60  # Convert to degrees on the color circle
-        if hue < 0:
-            hue += 360
-        
-        # Convert to range [0, 180]
-        hue /= 2
-        return hue
-
-    def hue_hist(image):
-        # Convert to float and split channels
-        image = image.astype(np.float32)
-        b, g, r = cv2.split(image)
-
-        # Calculate Hue for each pixel
-        hue_values = np.vectorize(rgb_to_hue)(r, g, b)
-
-        # Calculate histogram
-        hist, bin_edges = np.histogram(hue_values, bins=7, range=(0, 180))
-        return hist
-    
-    return hue_hist(image)
-
-# def calculate_hue_histogram_from_RGB(img):
-#     R, G, B = img[:, :, 0], img[:, :, 1], img[:, :, 2]
-#     normR, normG, normB = R / 255.0, G / 255.0, B / 255.0
-#     Cmax = np.max([normR, normG, normB], axis=0)
-#     Cmin = np.min([normR, normG, normB], axis=0)
-#     delta = Cmax - Cmin
-
-#     # Calculate hue
-#     hue = np.zeros_like(delta)
-#     hue[Cmax == normR] = 60 * (((normG - normB) / delta[Cmax == normR]) % 6)
-#     hue[Cmax == normG] = 60 * (((normB - normR) / delta[Cmax == normG]) + 2)
-#     hue[Cmax == normB] = 60 * (((normR - normG) / delta[Cmax == normB]) + 4)
-#     hue[delta == 0] = 0
-
-#     # Calculate histogram
-#     hue_histogram, _ = np.histogram(hue, bins=7, range=(0, 180))
-#     return hue_histogram
-
-
-# from sklearn.cluster import MiniBatchKMeans
-# def calculate_hue_histogram(img, n_colors=7, n_init=3):
-#     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-#     pixels = hsv_img[:, :, 0].reshape(-1, 1)
-#     kmeans = MiniBatchKMeans(n_clusters=n_colors, n_init=n_init)
-#     labels = kmeans.fit_predict(pixels)
-#     hue_histogram, _ = np.histogram(labels, bins=np.arange(0, n_colors+1))
-#     return hue_histogram
-
 
 def process_image(data):
     index, image_path = data
     if index % 100 == 0:
         print(f"Processing image at index: {index}")
     
-    # print(image_path)
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Feature extraction
-    # keypoints = cv2.goodFeaturesToTrack(gray, maxCorners=200, qualityLevel=0.02, minDistance=5)
-    # n_keypoints = keypoints.shape[0] if keypoints is not None else 0
+    # Keypoints and descriptors
     keypoints, descriptors = calculate_keypoints_and_descriptors(gray)
     n_keypoints = len(keypoints) if keypoints is not None else 0
-
-    # avg_brightness = calculate_brightness(gray)
-    # brightness_rms = calculate_rms(gray)
-
-    # # Calculate perceived brightness
-    # perc_brightness_avg = calculate_perceived_brightness_avg(image)
-    # perc_brightness_rms = calculate_perceived_brightness_rms(image)
-
+    
+    # Calculate average and RMS brightness from grayscale image
     avg_brightness = calculate_perceived_brightness(gray, 'avg')
     brightness_rms = calculate_perceived_brightness(gray, 'rms')
 
-    # Calculate perceived brightness for RGB image
+    # Calculate average and RMS of perceived brightness from RGB image
     perc_brightness_avg = calculate_perceived_brightness(image, 'avg')
     perc_brightness_rms = calculate_perceived_brightness(image, 'rms')
 
+    # Calculate contrast
     contrast = calculate_contrast(gray)
 
+    # Calculate edge length histogram
     edges = calculate_edges_new(image)
     edge_len_features = {f'edge_length{i+1}': edges[i] for i in range(len(edges))}
+    # Calculate edge angle histogram
     edge_angles = calculate_edge_angles(image)
     edge_angle_features = {f'edge_angle{i+1}': edge_angles[i] for i in range(len(edge_angles))}
-
-    # edge_length_histogram, edge_angle_histogram = calculate_edge_features(image)
-    # edge_len_features = {f'edge_length{i+1}': edge_length_histogram[i] for i in range(len(edge_length_histogram))}
-    # edge_angle_features = {f'edge_angle{i+1}': edge_angle_histogram[i] for i in range(len(edge_angle_histogram))}
 
     contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     main_contour = max(contours, key=cv2.contourArea) if contours else None
     aspect_ratio = calculate_aspect_ratio(main_contour) if main_contour is not None else 0
     area_by_perim = calculate_area_by_perimeter(main_contour) if main_contour is not None else 0
 
+    # Calculate hue histogram
     hue_histogram = calculate_hue_histogram(image)
-    # hue_histogram = calculate_hue_histogram_RGB(image)
     hue_features = {f'hue{i+1}': hue_histogram[i] for i in range(len(hue_histogram))}
 
     return {
@@ -286,19 +132,10 @@ dataset_dir = '../../../images/ILSVRC/Data/CLS-LOC/val/'
 
 # Assuming images are directly under the dataset directory
 image_paths = [os.path.join(dataset_dir, filename) for filename in os.listdir(dataset_dir)]
-image_paths = image_paths[:1000]  # Example with a subset of 1000 images
+# image_paths = image_paths[:1000]  # Example with a subset of 1000 images
 
 # Parallel processing
 from concurrent.futures import ProcessPoolExecutor
-
-# def process_image_parallel(image_paths, max_workers=4):
-#     with ProcessPoolExecutor(max_workers=max_workers) as executor:
-#         results = list(executor.map(process_image, image_paths))
-#     return results
-
-# # Extract features for each image in parallel
-# features_list = process_image_parallel(image_paths, max_workers=8)  # Adjust max_workers as needed
-
 
 # Pair each image path with its index
 image_paths_with_index = list(enumerate(image_paths))
@@ -310,7 +147,6 @@ def process_image_parallel(image_data, max_workers=4):
 
 # Extract features for each image in parallel
 features_list = process_image_parallel(image_paths_with_index, max_workers=8)  # Adjust max_workers as needed
-
 
 # Convert to DataFrame and save to CSV
 df = pd.DataFrame(features_list)
--- code/feature-engineering/mt_feat_ext.py
+++ code/feature-engineering/mt_feat_ext.py
@@ -2,12 +2,10 @@ import pandas as pd
 import spacy
 from nltk.corpus import wordnet as wn
 from nltk.tokenize import word_tokenize
-from tokenizers import ByteLevelBPETokenizer
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
 import string
 
-# Function to initialize and return a CountVectorizer model
 def create_bow_model(sentences):
     vectorizer = CountVectorizer()
     vectorizer.fit(sentences)
@@ -17,31 +15,26 @@ def is_satellite_adjective(word):
     synsets = wn.synsets(word, pos=wn.ADJ_SAT)
     return bool(synsets)
 
-def extract_features(sentence, bow_vectorizer, bpe_tokenizer):
+def extract_features(sentence, bpe_sentence, bow_vectorizer):
     doc = nlp(sentence)
-    # Filtered tokens list: includes only words
     word_tokens = [token.text for token in doc if token.is_alpha]
-    # Original tokens list: includes words, punctuation, and symbols
     tokens = [token.text for token in doc]
-    # Part-of-speech counts
     pos_counts = Counter(token.pos_ for token in doc if token.is_alpha)
 
-    # n_words - count of actual words in the sentence
-    # n_tokens - tokens can include words, punctuation, symbols, and other elements
     n_words = len(word_tokens)
     n_tokens = len(word_tokenize(sentence))
     avg_noun = pos_counts['NOUN'] / n_words if n_words else 0
     avg_verb = pos_counts['VERB'] / n_words if n_words else 0
     avg_adj = pos_counts['ADJ'] / n_words if n_words else 0
-    # satellite_adjs - https://stackoverflow.com/a/18817481
     satellite_adjs = sum(1 for word in word_tokens if is_satellite_adjective(word))
     avg_sat_adj = satellite_adjs / n_words if n_words else 0
     avg_adverb = pos_counts['ADV'] / n_words if n_words else 0
     avg_punc = sum(1 for char in sentence if char in string.punctuation) / n_words if n_words else 0
     avg_word_length = sum(len(word) for word in word_tokens) / n_words if n_words else 0
 
-    bpe_tokens = bpe_tokenizer.encode(sentence)
-    n_bpe_chars = len(bpe_tokens.tokens)
+    # BPE tokenization from the bpe file
+    bpe_tokens = bpe_sentence.split()
+    n_bpe_chars = len(bpe_tokens)
     avg_bpe = n_bpe_chars / n_words if n_words else 0
     
     feature_dict = {
@@ -59,72 +52,42 @@ def extract_features(sentence, bow_vectorizer, bpe_tokenizer):
     }
 
     bow = bow_vectorizer.transform([sentence]).toarray()[0]
-    # Add BoW features
     for i, val in enumerate(bow):
         feature_dict[f'bow_{i}'] = val
 
     return feature_dict
 
-
-base_dir = "/home/drew99/School/MLDS2/wmt16de"
-
 # Load Spacy's model for part-of-speech tagging
 nlp = spacy.load('en_core_web_sm')
 
-# Ensure WordNet and NLTK's punkt are downloaded
-# import nltk
-# nltk.download('punkt')
-# nltk.download('wordnet')
+base_dir = "/home/drew99/School/MLDS2/wmt16de"
 
-
-import json
-
-# Path to the BPE vocabulary file
-vocab_file_path = f'{base_dir}/vocab.bpe.32000'
-# Path to save the JSON-formatted vocabulary file
-json_vocab_file_path = f'{base_dir}/vocab.json'
-
-def convert_vocab_to_json(vocab_file_path, json_vocab_file_path):
-    with open(vocab_file_path, 'r', encoding='utf-8') as file:
-        vocab = {line.strip(): idx for idx, line in enumerate(file)}
-
-    with open(json_vocab_file_path, 'w', encoding='utf-8') as json_file:
-        json.dump(vocab, json_file, ensure_ascii=False)
-
-convert_vocab_to_json(vocab_file_path, json_vocab_file_path)
-
-
-# Initialize BPE tokenizer
-# bpe_tokenizer = ByteLevelBPETokenizer('path/to/vocab', 'path/to/merges')
-bpe_tokenizer = ByteLevelBPETokenizer(
-    f'{base_dir}/vocab.json', 
-    f'{base_dir}/bpe.32000'
-)
-
-# Update paths to where the training files are located
+# Loading sentences from files
 english_file_path = f'{base_dir}/train.en'
-german_file_path = f'{base_dir}/train.de'
-# # Paths to the tokenized and cleaned training files
-# english_file_path = f'{base_dir}/train.tok.clean.en'
-# german_file_path = f'{base_dir}/train.tok.clean.de'
+bpe_file_path = f'{base_dir}/train.tok.bpe.32000.en'
 
-# Load the first 5000 sentences from the English file
+# Load sentences
+with open(english_file_path, 'r', newline='\n', encoding='utf-8') as file_en, open(bpe_file_path, 'r', encoding='utf-8') as file_bpe:
+    english_sentences = [line.strip() for line in file_en]
+    bpe_sentences = [line.strip() for line in file_bpe]
+
+# Ensure the number of sentences in both files are the same
+# print(f"Number of sentences in English file: {len(english_sentences)}")
+# print(f"Number of sentences in BPE file: {len(bpe_sentences)}")
+assert len(english_sentences) == len(bpe_sentences)
+
+# Limit the number of sentences for testing
 num_sentences = 5000
-english_sentences = []
-with open(english_file_path, 'r', encoding='utf-8') as file_en:
-    for i, line in enumerate(file_en):
-        if i < num_sentences:
-            english_sentences.append(line)
-        else:
-            break
+english_sentences = english_sentences[:num_sentences]
+bpe_sentences = bpe_sentences[:num_sentences]
+# print(english_sentences[:5], "\n", bpe_sentences[:5])
 
-# english_sentences = english_sentences[:5000]
 # Create a BoW model based on the English sentences
 bow_vectorizer = create_bow_model(english_sentences)
 
 # Extract features from English sentences
-features_list = [extract_features(sentence, bow_vectorizer, bpe_tokenizer) for sentence in english_sentences]
+features_list = [extract_features(eng_sent, bpe_sent, bow_vectorizer) for eng_sent, bpe_sent in zip(english_sentences, bpe_sentences)]
 
 # Convert to DataFrame and save to CSV
 df = pd.DataFrame(features_list)
-df.to_csv('wmt16_features.csv', index=False)
+df.to_csv('./wmt16_features.csv', index=False)
