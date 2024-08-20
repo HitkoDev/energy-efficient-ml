544d8e719405f937689302f5ccf2e5f5ffd1d1ac
71M073J <ciglarictimotej@gmail.com>
2024-08-20 19:40:24 +0200

    Last working version, premodels work for images

--- code/image-classification/premodel/main.py
+++ code/image-classification/premodel/main.py
@@ -1,33 +1,13 @@
 import tensorflow as tf
 import tensorflow_hub as hub
-
+import time
 from sklearn import svm
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeRegressor
-
+import cv2
 ## Fetch pretrained models from the paper (all models trained on ILSVRC-2012-CLS)
 
-mobilenet_v1_25 = tf.keras.Sequential([
-    hub.KerasLayer("https://www.kaggle.com/models/google/mobilenet-v1/frameworks/TensorFlow2/variations/025-224-classification/versions/2")
-])
-mobilenet_v1_25.build([None, 224, 224, 3])
-#TEST
-#mobilenet_v1_25(tf.ones((16, 224, 224, 3)))
-resnet_v1_50 = tf.keras.Sequential([
-    hub.KerasLayer("https://www.kaggle.com/models/google/resnet-v1/frameworks/TensorFlow2/variations/152-classification/versions/2")
-])
-resnet_v1_50.build([None, 224, 224, 3])
-
-inception_v2 = tf.keras.Sequential([
-    hub.KerasLayer("https://www.kaggle.com/models/google/inception-v2/frameworks/TensorFlow2/variations/classification/versions/2")
-])
-inception_v2.build([None, 224, 224, 3])
-
-resnet_v2_152 = tf.keras.Sequential([
-    hub.KerasLayer("https://www.kaggle.com/models/google/resnet-v2/frameworks/TensorFlow2/variations/152-classification/versions/2")
-])
-resnet_v2_152.build([None, 224, 224, 3])
 
 def make_DT(X_train, X_test, Y_train):
     """
@@ -79,3 +59,46 @@ def KNN(X_train, X_test, Y_test):
     predicted = knn.predict(Y_test)
     return predicted
 
+if __name__ == "__main__":
+
+    batch = [1,8,32, 64]
+    for b in batch:
+        print("batch:", b)
+        for model in range(4):
+            if model == 0:
+                print("mobilenetv1")
+                model = tf.keras.Sequential([
+                    hub.KerasLayer(
+                        "https://www.kaggle.com/models/google/mobilenet-v1/frameworks/TensorFlow2/variations/025-224-classification/versions/2")
+                ])
+                model.build([None, 224, 224, 3])
+            # TEST
+            # mobilenet_v1_25(tf.ones((16, 224, 224, 3)))
+            elif model == 4:
+                model = tf.keras.Sequential([
+                    hub.KerasLayer(
+                        "https://www.kaggle.com/models/google/resnet-v1/frameworks/TensorFlow2/variations/152-classification/versions/2")
+                ])
+                model.build([None, 224, 224, 3])
+            elif model == 2:
+                print("inceptionv2")
+                model = tf.keras.Sequential([
+                    hub.KerasLayer(
+                        "https://www.kaggle.com/models/google/inception-v2/frameworks/TensorFlow2/variations/classification/versions/2")
+                ])
+                model.build([None, 224, 224, 3])
+            elif model == 3:
+                print("resnet152")
+                model = tf.keras.Sequential([
+                    hub.KerasLayer(
+                        "https://www.kaggle.com/models/google/resnet-v2/frameworks/TensorFlow2/variations/152-classification/versions/2")
+                ])
+                model.build([None, 224, 224, 3])
+            if type(model) != int:
+
+                img = cv2.resize(cv2.imread("./val_1/ILSVRC2012_val_00000001.JPEG"), (224, 224), interpolation=cv2.INTER_LINEAR)
+                img = np.tile(img[None, :, :, :], (b, 1, 1, 1))
+                t1 = time.time()
+                model(img)
+                t2 = time.time()
+                print((t2 - t1)/b, "seconds")
--- code/image-classification/premodel/premodel.py
+++ code/image-classification/premodel/premodel.py
@@ -16,6 +16,8 @@ from math import ceil
 import util
 from threading import Thread
 
+
+
 IMG_SIZE = 228
 # Percentage_TESTED = 10
 # DATA_FILE = 'all_image_features.csv'
@@ -362,7 +364,7 @@ def CV_fold_worker(test_idx, train_idx, img_data, first_level, second_level, thi
 
 def process(data, first_level_data, second_level_data, third_level_data, list_premodels, comb):
     counter, (first_level_machine, second_level_machine, third_level_machine) = comb
-    k_fold = 2
+    k_fold = 10
     all_predictions = list()
     chunk_size = int(ceil(len(data) / float(k_fold)))
     # Create a new thread for each fold
@@ -373,25 +375,39 @@ def process(data, first_level_data, second_level_data, third_level_data, list_pr
                               first_level_machine, second_level_machine, third_level_machine, return_wrapper,
                               i + counter * len(data) // 2)
         all_predictions.extend(return_wrapper)
-    t_0 /= 2
+    #t_0 /= 2
 
     predicted = []
     correct_result = []
     result_prediction = []
+    predicted_models = []
     for p in all_predictions:
         for image, groundtruth_label, result_pred, prediction, model_predicted in p:
             correct_result.append(groundtruth_label)
-            predicted.append(prediction)
+            predicted.append(prediction) #<- this is model index
             result_prediction.append(result_pred)
+            predicted_models.append(model_predicted)#<- this is model name
     time_elapsed = f"Time elapsed: {int(t_0 * 100) / 100} seconds"
     #res = (
     #    np.sum(np.array(predicted) == np.array(correct_result)) / len(predicted), list_premodels[counter], time_elapsed)
 
     #############################################################################################################################
     #Optimal model selector - prediction has to agree with nn (all nn's predict the same on val data - can use first level data)#
-
-    res = (
-        np.sum([int(first_level_data[x][1]) == int(result_prediction[x]) for x in range(len(first_level_data))]) / len(predicted), list_premodels[counter], time_elapsed)
+    #res = (np.sum(np.array(predicted) == np.array(correct_result)) / len(predicted), list_premodels[counter], time_elapsed)
+    lookup = {"tf-mobilenet_v1":0.1, "tf-inception_v4":0.2, "tf-resnet_v1_152":0.6, "failed":0.0}
+    time_with_nn = sum([lookup[x] for x in predicted_models])
+    time_best_nn = len(predicted_models) * 0.6
+    improvement = f"Best_nn: {time_best_nn} seconds,  premodel+nn time: {time_with_nn + t_0}, seconds"
+    acc = 0
+    n_correct = 0
+    predicted_correct = 0
+    for i, (gt, pred, res, model) in enumerate(zip(correct_result, predicted, result_prediction, predicted_models)):
+        if int(first_level_data[i][1]) == gt: #oracle predictor
+            n_correct += 1
+            if gt == res:
+                predicted_correct += 1
+    #np.sum([int(first_level_data[x][1]) == int(result_prediction[x]) for x in range(len(first_level_data))]) / len(predicted)
+    res = (predicted_correct/n_correct, list_premodels[counter], time_elapsed, improvement, predicted_correct/len(predicted_models))
 
     return res
 
@@ -429,37 +445,8 @@ def prototype(amount_images, list_premodels):
 
 
 if __name__ == "__main__":
-    # inference times, without other processes
-    a = [(0.6693533870677414, ['dt16', 'dt16', 'dt16'], 'Time elapsed: 0.0 seconds'),
-         (0.7003540070801416, ['dt16', 'dt16', 'knn'], 'Time elapsed: 0.05 seconds'),
-         (0.6992539850797016, ['dt16', 'knn', 'dt16'], 'Time elapsed: 0.07 seconds'),
-         (0.6994539890797816, ['dt16', 'knn', 'knn'], 'Time elapsed: 0.09 seconds'),
-         (0.7029940598811977, ['knn', 'dt16', 'dt16'], 'Time elapsed: 0.6 seconds'),
-         (0.7024340486809736, ['knn', 'dt16', 'knn'], 'Time elapsed: 0.62 seconds'),
-         (0.7024740494809896, ['knn', 'knn', 'dt16'], 'Time elapsed: 0.67 seconds'),
-         (0.6438928778575571, ['knn', 'knn', 'knn'], 'Time elapsed: 0.81 seconds'),
-         (0.7075341506830136, ['dt16', 'knn', 'svc'], 'Time elapsed: 1.4 seconds'),
-         (0.7109142182843657, ['knn', 'dt16', 'svc'], 'Time elapsed: 1.93 seconds'),
-         (0.7071541430828616, ['dt16', 'dt16', 'svc'], 'Time elapsed: 6.54 seconds'),
-         (0.7075141502830057, ['dt16', 'svc', 'svc'], 'Time elapsed: 6.72 seconds'),
-         (0.7077741554831096, ['dt16', 'svc', 'knn'], 'Time elapsed: 6.89 seconds'),
-         (0.7075141502830057, ['dt16', 'svc', 'dt16'], 'Time elapsed: 7.04 seconds'),
-         (0.7108142162843257, ['knn', 'svc', 'dt16'], 'Time elapsed: 10.55 seconds'),
-         (0.7105542110842217, ['knn', 'svc', 'svc'], 'Time elapsed: 10.68 seconds'),
-         (0.7105542110842217, ['knn', 'svc', 'knn'], 'Time elapsed: 10.84 seconds'),
-         (0.7106142122842457, ['knn', 'knn', 'svc'], 'Time elapsed: 11.12 seconds'),
-         (0.7068741374827496, ['svc', 'svc', 'svc'], 'Time elapsed: 46.47 seconds'),
-         (0.7076141522830457, ['svc', 'svc', 'dt16'], 'Time elapsed: 49.23 seconds'),
-         (0.7074741494829897, ['svc', 'svc', 'knn'], 'Time elapsed: 51.05 seconds'),
-         (0.7076141522830457, ['svc', 'dt16', 'svc'], 'Time elapsed: 51.35 seconds'),
-         (0.7074341486829736, ['svc', 'knn', 'svc'], 'Time elapsed: 59.86 seconds'),
-         (0.7075941518830376, ['svc', 'dt16', 'dt16'], 'Time elapsed: 63.79 seconds'),
-         (0.7074341486829736, ['svc', 'knn', 'knn'], 'Time elapsed: 64.06 seconds'),
-         (0.7076541530830617, ['svc', 'dt16', 'knn'], 'Time elapsed: 64.12 seconds'),
-         (0.7076941538830777, ['svc', 'knn', 'dt16'], 'Time elapsed: 64.23 seconds')]
     import matplotlib.pyplot as plt
 
-    print(a)
     # plt.plot(a)
     # plt.show()
     # quit()
@@ -470,18 +457,17 @@ if __name__ == "__main__":
     print(models)
 
     # amount_images = int(input("How many images do you want to use? "))
-    amount_images = 50000
+    amount_images = 10000
     # list_premodels = input("Which models do you want to use? ")
     list_premodels = [('knn', 'knn', 'knn'), ('dt16', 'dt16', 'dt16'), ('svc', 'svc', 'svc')]
 
     # percetange_results = prototype(amount_images, list_premodels)
-    useSavedResults = False
-    if not useSavedResults:
-        percetange_results, best_premodel = prototype(amount_images,
-                                                      [[y for y in x.split() if y != "-"] for x in models])
-    else:
-        percetange_results = a
+    saveImg = True
+    percetange_results, best_premodel = prototype(amount_images,
+                                                  [[y for y in x.split() if y != "-"] for x in models])
+
     print("Results: ")
+    print(percetange_results)
     #percetange_results.sort(key=lambda x: float(x[2].split()[2]))
     percetange_results.sort(key=lambda x: x[0])
     if amount_images == 50000:
@@ -490,20 +476,27 @@ if __name__ == "__main__":
             print(percetange_results, file=f)
             print(percetange_results)
     times = [float(x[2].split()[2]) for x in percetange_results]
+    times2 = [float(x[3].split()[5][:-1]) for x in percetange_results]
+    print(times2)
     fig, host = plt.subplots(figsize=(8, 5), layout="constrained")
     ax2 = host.twinx()
     ax2.set_ylim(0.0, 0.9)
     host.set_ylim(0, 1.1)
-    host.bar(range(len(percetange_results)), [x / np.max(times) for x in times], label="Relative prediction time")
+    #host.bar(range(len(percetange_results)), [x / np.max(times) for x in times], label="Relative prediction time")
+    host.bar(range(len(percetange_results)), [x / np.max(times2) for x in times2], label="Relative prediction time")
     p1 = host.plot(0, 0, label="Relative prediction time", color="b")
     host.set_xticks(range(len(percetange_results)), [str(x[1]) for x in percetange_results], rotation=90)
-    p2 = ax2.plot(range(len(percetange_results)), [x[0] for x in percetange_results], "o-", color="r", label="Accuracy")
-    host.set_ylabel("Relative prediction time")
+    maxval = max(times2)/amount_images
+    host.set_yticks([0,0.2, 0.4, 0.6, 0.8, 1], [str(maxval*(i/5))[:4] for i in range(6)], rotation=90)
+    p2 = ax2.plot(range(len(percetange_results)), [x[0] for x in percetange_results], ".-", color="r", label="Accuracy compared to Oracle")
+    host.set_ylabel("Relative prediction time (s)")
     ax2.set_ylabel("Accuracy")
-    host.set_yticks([0])
+    #host.set_yticks([0])
     #ax2.legend()
     ax2.grid()
     ax2.legend(handles=p1+p2)
     plt.tight_layout()
+    if saveImg:
+        plt.savefig(f"premodel_orderedByAcc_{amount_images}.png")
     plt.show()
     print("Finished")
